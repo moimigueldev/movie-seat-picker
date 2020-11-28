0e15b149025be3b98947c9f0d5853b74476bb288
moises <moimigueldev@gmail.com>
2020-11-28 15:35:12 -0600

    functionality to save to localstorage added

--- script.js
+++ script.js
@@ -2,15 +2,43 @@ const movieSelected = document.getElementById('movie');
 const seatsSelected = document.getElementById('seatsSelected');
 const totalPrice = document.getElementById('totalPrice');
 const container = document.querySelector('.container');
+const allSeats = document.querySelectorAll('.container .seat');
+
+const safeState = (seats) => {
+  const selectedSeats = [...seats].map((seat) => {
+    return [...allSeats].indexOf(seat);
+  });
+  localStorage.setItem('seats', JSON.stringify(selectedSeats));
+};
 
 const updatePrice = () => {
   const selectedSeats = document.querySelectorAll(
     '.container .seat.selected:not(.occupied)'
   );
+
   const selectedSeatsCount = selectedSeats.length;
 
   totalPrice.innerText = selectedSeatsCount * +movieSelected.value;
   seatsSelected.innerText = selectedSeatsCount;
+
+  safeState(selectedSeats);
+};
+
+const setSavedData = () => {
+  const savedMovie = localStorage.getItem('movieSelected');
+  const savedSeats = JSON.parse(localStorage.getItem('seats'));
+
+  if (savedMovie !== null) {
+    movieSelected.value = savedMovie;
+  }
+
+  if (savedSeats !== null) {
+    savedSeats.forEach((seat) => {
+      allSeats[seat].classList.add('selected');
+    });
+  }
+
+  updatePrice();
 };
 
 const selectSeat = (seat) => {
@@ -19,7 +47,7 @@ const selectSeat = (seat) => {
 };
 
 movieSelected.addEventListener('change', () => {
-  console.log('change');
+  localStorage.setItem('movieSelected', movieSelected.value);
   updatePrice();
 });
 
@@ -32,3 +60,5 @@ container.addEventListener('click', (e) => {
     selectSeat(e.target);
   }
 });
+
+setSavedData();
